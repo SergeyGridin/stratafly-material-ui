16e2e2be5456b428a8e86c0c56531711e26aef20
SergeyGridin <gridin21@gmail.com>
2020-11-10 16:39:07 -0500

    added header and themeprovider

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { Desktop } from '../components/Desktop';
+import { AppHeader } from '../components/AppHeader';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { Theme } from '../Theme';
 
 class App extends Component {
   render() {
-    return <Desktop />;
+    // if (window.screen.width >= 1024 && window.screen.height >= 768) return <Desktop />;
+    return (
+      <MuiThemeProvider theme={Theme}>
+        <AppHeader />
+      </MuiThemeProvider>
+    );
   }
 }
 export default App;
